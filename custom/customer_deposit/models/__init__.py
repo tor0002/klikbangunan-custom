83cc01f57c3d776c77124c0f85dfd45bf47b88b3
tor0002 <vihat04@gmail.com>
2024-10-04 12:08:05 +0700

    Hapus Draft Custom

--- custom/customer_deposit/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_partner
-from . import pos_payment_method
-from . import pos_order
-from . import pos_session
-from . import account_payment
--- custom/price_restricted_2/models/product_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, api, fields
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    min_price = fields.Float('Minimum Price')
-    max_price = fields.Float('Maximum Price')
