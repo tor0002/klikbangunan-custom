0545099e8134ed5efdbc14d0fedf6ad2400274e9
tor0002 <vihat04@gmail.com>
2024-10-14 10:54:45 +0700

    Update dikit

--- fix-custom/print_ds_from_batch/models/batch_transfer.py
+++ fix-custom/print_ds_from_batch/models/batch_transfer.py
@@ -1,9 +1,14 @@
-from odoo import models
+from odoo import models, api
+from odoo.exceptions import UserError
 
 class PickingBatch(models.Model):
     _inherit = 'stock.picking.batch'
 
     def action_print_delivery_slips(self):
+        # Memastikan state batch adalah 'done'
+        if self.state != 'done':
+            raise UserError('Cannot print delivery slips because the batch is not done.')
+        
         # Mendapatkan semua pickings yang ada di batch ini
         pickings = self.mapped('picking_ids')
         
