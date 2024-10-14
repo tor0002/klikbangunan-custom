642611a8fe1a7ce71b1b8c2925654e377e4474cd
tor0002 <vihat04@gmail.com>
2024-10-14 11:33:38 +0700

    Update

--- custom/ksi_delivery/models/models.py
+++
@@ -1,8 +0,0 @@
-# from odoo import models, fields
-
-# class StockPickingBatch(models.Model):
-#     _inherit = 'stock.picking.batch'
-
-#     driver_id = fields.Many2one('drivers', string='Driver')
-#     driver_name = fields.Char(related='driver_id.drivers_name', string='Driver Name', store=True)
-#     driver_store = fields.Selection(related='driver_id.drivers_store', string='Driver Store', store=True)
